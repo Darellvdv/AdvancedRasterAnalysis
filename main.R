eaaab581773c9c46ce0079f5905358cc183f1634
darellvdv <darellvdv@gmail.com>
2016-01-13 11:49:31 +0100

    Added to-do list

--- main.R
+++ main.R
@@ -2,4 +2,24 @@
 # TeamTropical - Darell.vanderVoort & Froede.Vrolijk
 # 13th of Janaury 2016
 
+# To do
+# 1: produce one or more plots that demonstrate the relationship between the Landsat bands and the VCF tree cover
+
+
+# 2: create an lm() model and show a summary (e.g. using summary()) of the model object you created. 
+     #Which predictors (bands) are probably most important in predicting tree cover?
+
+
+# 3: Plot the predicted tree cover raster and compare with the original VCF raster
+
+
+# 4: Compute the RMSE between your predicted and the actual tree cover values 
+
+
+# 5: are the differences between the predicted and actual tree cover the same for all of the 3 classes we used for the random forest classfication? 
+#    Using the training polygons from the random forest classification, calculate the RMSE separately for each of the classes and compare. 
+#    Hint - see ?zonal().
+
+
+
 # Load libraries
